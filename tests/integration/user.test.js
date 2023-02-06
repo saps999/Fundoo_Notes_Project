9bc31bce56f9c7548048419ff4cb0bfc314fbaf2
saps999 <saptarshibiswasgang@gmail.com>
2023-02-06 23:01:51 +0530

    [SAPTARSHI] Added : Testing user registration

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -25,4 +25,90 @@ describe('User APIs Test', () => {
 
     done();
   });
+
+  describe('User Registration ', () => {
+
+    it('ValidUserDetailsAreGivenItShouldBeSaveInDatabase', (done) => {
+      const inputBody = {
+        "fname": "Dhanshree",
+        "lname": "Patil",
+        "email": "patilds@gmail.com",
+        "password": "dhanshree"
+      }
+      request(app)
+        .post('/api/v1/users/')
+        .send(inputBody)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(201);
+          done();
+        });
+    });
+
+
+    it('InvalidFirstnameIsGivenItShouldThrowAnError', (done) => {
+      const inputBody = {
+        "fname": "ddd",
+        "lname": "maneg",
+        "email": "dhanu@gmail.com",
+        "password": "dhanshree2000"
+      }
+      request(app)
+        .post('/api/v1/users/')
+        .send(inputBody)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+
+
+    it('InvalidLastnameIsGivenItShouldThrowAnError', (done) => {
+      const inputBody = {
+        "fname": "Dhanshree",
+        "lname": "pat",
+        "email": "dhanshree@gmail.com",
+        "password": "dhanshree123"
+      }
+      request(app)
+        .post('/api/v1/users/')
+        .send(inputBody)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+
+    it('InvalidEmailIsGivenItShouldThrowAnError', (done) => {
+      const inputBody = {
+        "fname": "dhanshree",
+        "lname": "patil",
+        "email": "dhanshree@.com",
+        "password": "dhanshree"
+      }
+      request(app)
+        .post('/api/v1/users/')
+        .send(inputBody)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+
+
+    it('InvalidPasswordIsGivenItShouldThrowAnError', (done) => {
+      const inputBody = {
+        "fname": "dhanshree",
+        "lname": "patil",
+        "email": "dhanshree@gmail.com",
+        "password": 1234
+      }
+      request(app)
+        .post('/api/v1/users/')
+        .send(inputBody)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+  });
 });
